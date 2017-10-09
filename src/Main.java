1ba45d1daec90c343fb25b828d5a89664fa7340b
jumproper <jumproper36@gmail.com>
2017-10-08 23:23:13 -0400

    Actual infinite and random fractal behavior

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-import com.sun.org.apache.xpath.internal.SourceTree;
-import javafx.scene.effect.BlendMode;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
@@ -22,21 +20,23 @@ import static java.awt.Color.white;
  * Created by jumpr on 8/19/2016.
  */
 public class Main {
+    public static final int FRAMES = 340;
+
+    private static double scale=1.0;
+    private static double rotate = 0.0;
+    private static double zoomFac=scale;
+    private static double offsetX=0.0;
+    private static double offsetY = 0.0;
     public static void main(String args[]) throws Exception {
         ArrayList<BufferedImage> sequence = new ArrayList<>();
         String path;
-        for(int j=0;j<500;j++){
-            path="C:\\tmp\\Feedback\\Input\\test_"+String.format("%06d", j)+".png";
-            if(j%10==0) {
-                System.out.println((100 * (j / 500.0) + "%"));
-            }
-            sequence.add(ImageIO.read(new File(path)));
-        }
-        BufferedImage img = ImageIO.read(new File("image.png"));
-        BufferedImage output=copyImage(sequence.get(0));
-        int iter=1000;
+        path="input";
+        for(File f: new File("seq/").listFiles())
+            f.delete();
+        File[] listOfFiles = new File(path).listFiles();
+        BufferedImage output=copyImage(ImageIO.read(listOfFiles[0]));
         int rand=(int)(Math.random()*1000);
-        feedbackSequence(sequence,output,0,rand);
+        feedbackSequence(listOfFiles,output,0,rand);
         File outputfile = new File("output.jpg");
         ImageIO.write(output, "png", outputfile);
     }
@@ -60,8 +60,8 @@ public class Main {
         temp=scaleOp.filter(output,temp);
         Graphics2D g2=output.createGraphics();
         g2.drawImage(temp,0,0,null);
-        mirrorImage(output,false,rand);
-        mirrorImage(output,true,rand);
+        mirrorImage(output,false,rand,j);
+        mirrorImage(output,true,rand,j);
         File outputfile = new File("C:\\tmp\\Feedback\\"+j+".png");
         try {
             ImageIO.write(output, "png", outputfile);
@@ -70,39 +70,69 @@ public class Main {
         }
         return feedback(img,output,iter,(j+1),rand);
     }
-    public static BufferedImage feedbackSequence(ArrayList<BufferedImage> img, BufferedImage output,int j, int rand){
-        if(j==img.size()){
+    public static BufferedImage feedbackSequence(File[] imgSequence, BufferedImage output,int j, int rand){
+        if(j==imgSequence.length){
             return output;
         }
-        int w=img.get(j).getWidth();
-        int h=img.get(j).getHeight();
-        int iter=img.size();
-        System.out.println("j="+j);
-        double scale=1.0-getAngleCoefficient(iter,j,8,true)*(0.5+j)/iter;
-        if((int)(w*scale)==0||(int)(h*scale)==0){
-            return output;
-        }
-        BufferedImage temp=copyImage(img.get(j));
-        AffineTransform at = new AffineTransform();
-        at.translate(w/2+5,h/2-5);
-        at.scale(scale,scale);
-        at.rotate(Math.PI/4/iter*j);
-
-        at.translate(-w/2,-h/2);
-        AffineTransformOp scaleOp =
-                new AffineTransformOp(at, AffineTransformOp.TYPE_BILINEAR);
-        temp=scaleOp.filter(output,temp);
-        Graphics2D g2=output.createGraphics();
-        g2.drawImage(temp,0,0,null);
-        mirrorImage(output,false,rand);
-        mirrorImage(output,true,rand);
-        File outputfile = new File("C:\\tmp\\Feedback\\"+j+".png");
+        BufferedImage currentFrame= null;
+        File[] outputs = new File("seq/").listFiles();
         try {
-            ImageIO.write(output, "png", outputfile);
+            currentFrame = ImageIO.read(imgSequence[j]);
+            int w = currentFrame.getWidth();
+            int h = currentFrame.getHeight();
+            int iter = imgSequence.length;
+            scale -= Math.sin(j/iter) / 200;
+            rotate -= Math.cos(j /iter) / 100;
+            zoomFac -= Math.cos(j /iter) / 200;
+            offsetX -= Math.sin(j /iter) / 200;
+            offsetY -= Math.cos(j /iter) / 200;
+            System.out.println("j=" + j);
+            System.out.println("rotate=" + rotate);
+            System.out.println("zoom=" + zoomFac);
+            System.out.println("offset="+offsetY+" : "+offsetY);
+            BufferedImage temp = new BufferedImage(w,h,BufferedImage.TYPE_INT_ARGB);
+            if(j>0 && j-1<outputs.length) {
+                try {
+                    temp = ImageIO.read(outputs[j - 1]);
+                }
+                catch (Exception e){
+                    System.out.println(e.toString() + Arrays.toString(outputs));
+                    return feedbackSequence(imgSequence,currentFrame,(j),rand);
+                }
+            }
+            AffineTransform at = new AffineTransform();
+            at.translate(w / 2 + offsetX, h / 2 + offsetY);
+            at.scale(scale, scale);
+            at.rotate(rotate);
+
+            at.translate(-w / 2, -h / 2);
+            AffineTransformOp scaleOp =
+                    new AffineTransformOp(at, AffineTransformOp.TYPE_BILINEAR);
+            temp = scaleOp.filter(output, temp);
+            AffineTransform zoom = new AffineTransform();
+            zoom.translate(w / 2 - offsetX, h / 2 - offsetY);
+            zoom.scale(zoomFac, zoomFac);
+            zoom.translate(-w / 2, -h / 2);
+            AffineTransformOp zoomOp = new AffineTransformOp(zoom, AffineTransformOp.TYPE_BILINEAR);
+            output = zoomOp.filter(currentFrame, output);
+            Graphics2D g2 = currentFrame.createGraphics();
+            g2.setComposite(AlphaComposite.SrcOver.derive(0.75f));
+            g2.drawImage(output, 0, 0, null);
+            g2.drawImage(temp, 0, 0, null);
+            g2.dispose();
+            mirrorImage(currentFrame, false, rand, h / 4);
+            mirrorImage(currentFrame, true, rand, w / 4);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        File outputfile = new File("seq/"+j+".png");
+        try {
+            ImageIO.write(currentFrame, "png", outputfile);
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return feedbackSequence(img,output,(j+1),rand);
+        return feedbackSequence(imgSequence,currentFrame,(j+1),rand);
     }
     public static double getAngleCoefficient(int iter,int j,int max, boolean inverse){
         if(inverse){
@@ -110,7 +140,7 @@ public class Main {
         }
         return max-max*(1.0*j/iter);
     }
-    public static void mirrorImage(BufferedImage img, boolean vert, int rand){
+    public static void mirrorImage(BufferedImage img, boolean vert, int rand,int offset){
         int h;
         int w;
         if(!vert){
@@ -131,14 +161,17 @@ public class Main {
                 //get source pixel value
                 int mc = (alpha << 27) | 0x00ffffff;
                 int p;
-                int o;
                 if (vert){
-                    o = img.getRGB(y, rx);
-                    p = img.getRGB(y, rx) - rand;
+                    p = img.getRGB(y, rx);
                 }
                 else {
-                    o = img.getRGB(rx, y);
-                    p = img.getRGB(rx, y) - rand;
+                    if(rx-offset<1){
+                        p = img.getRGB(rx+offset, y);
+                    }
+                    else{
+                        p = img.getRGB(rx-offset, y);
+                    }
+
                 }
 
                 //set mirror image pixel value - both left and right
